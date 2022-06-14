e162b984c68f7ac0b21095f7ec6049d2415b0e26
aNz <107313945+anzclan@users.noreply.github.com>
2022-06-14 13:52:53 +0800

    Update installer.sh

--- bot_panel/installer.sh
+++ bot_panel/installer.sh
@@ -138,7 +138,7 @@ EOF
         echo -e "Info...\n"
         fun_bot1() {
             [[ ! -e "/etc/.maAsiss/.Shellbtsss" ]] && {
-				wget -qO- https://scrzoke.000webhostapp.com/crud/ShellBot.sh >/etc/.maAsiss/.Shellbtsss
+				wget -qO- https://raw.githubusercontent.com/anzclan/scvpnme/main/bot_panel/ShellBot.sh >/etc/.maAsiss/.Shellbtsss
 			}
 			[[ "$(grep -wc "scvps_bot" "/etc/rc.local")" = '0' ]] && {
 			    sed -i '$ i\screen -dmS scvps_bot bbt' /etc/rc.local >/dev/null 2>&1
