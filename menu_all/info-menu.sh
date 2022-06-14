7f887e827276e456a3064d4d78a0eff66ac5c114
aNz <107313945+anzclan@users.noreply.github.com>
2022-06-14 22:48:19 +0800

    Update info-menu.sh

--- menu_all/info-menu.sh
+++ menu_all/info-menu.sh
@@ -93,7 +93,7 @@ echo -e "\033[1;32mAbout\033[0m :
    Time     : $WKT
    IPVPS    : $IPVPS
    
-   \033[1;33mThis script by : DBoyageVPN | t.me/donboyage\033[0m
+   \033[1;33mThis script by : SCVPN | t.me/anzclan\033[0m
 "
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m
 \033[1;32mPort Information\033[0m :
