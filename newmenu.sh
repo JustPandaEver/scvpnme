9d813ecd046d04c3ec440775f3311a2842972d3c
aNz <107313945+anzclan@users.noreply.github.com>
2022-06-15 01:11:51 +0800

    Update newmenu.sh

--- newmenu.sh
+++ newmenu.sh
@@ -107,12 +107,13 @@ echo -e "
  [\033[1;36m44\033[0m] • Server Status
  [\033[1;36m55\033[0m] • Trial Generator
  [\033[1;36m66\033[0m] • Logs User Created
- [\033[1;36m77\033[0m] • VPS Setting             [ Menu ]
- [\033[1;36m88\033[0m] • Autokill Multi-login    [ Menu ]
- [\033[1;36m99\033[0m] • ALL Information VPS     [ Menu ]
+ [\033[1;36m77\033[0m] • VPS Setting [ Menu ]
+ [\033[1;36m88\033[0m] • Autokill Multi-login [ Menu ]
+ [\033[1;36m99\033[0m] • ALL Information VPS[ Menu ]
 
- [\033[1;36m100\033[0m] • SYSTEM / Admin         [ Menu ]
+ [\033[1;36m100\033[0m] • SYSTEM / Admin [ Menu ]
  [\033[1;36m700\033[0m] • Bot-Panel $sts
+ [\033[1;36m800\033[0m] • About Script
 "
 if [[ $(cat /opt/.ver) = $serverV ]] > /dev/null 2>&1; then
 echo -ne
@@ -176,6 +177,9 @@ if [[ $(cat /opt/.ver) = $serverV ]] > /dev/null 2>&1; then
        system-menu
     elif [[ $x -eq 700 ]]; then
        installbot
+    elif [[ $x -eq 800 ]]; then
+       clear
+       about
     else
        menu
     fi
